b0331aa5ec19a47b05b4c9064e696caccca13af8
Josue <josuef@umich.edu>
2021-02-10 16:34:43 -0500

    exercise 1 for ps3

--- myJS.js
+++ myJS.js
@@ -9,24 +9,26 @@
 * Then write a function that changes the text and the color inside the div
 *
 */
-
+const block = document.getElementById('color-block');
 function changeColor(){
     //Write a condition determine what color it should be changed to
-    if(){
+    if(block.getAttribute('class') === 'newColor'){
         //change the background color using JS
-
+        block.setAttribute('class', "");
         //Change the text of the color using the span id color-name
-
+        const span = document.getElementById('color-name');
+        span.textContent = '#F08080';
     }
     else{
         //change the background color using JS
-
+        block.setAttribute('class', 'newColor');
         //Change the text of the color using the span id color-name
-
+        const span = document.getElementById('color-name');
+        span.textContent = '#A2BCDD';
 
     }
 }
-
+block.addEventListener('click', changeColor);
 
 /*
 * For excercise 2, you need to write an event handler for the button id "convertbtn"
